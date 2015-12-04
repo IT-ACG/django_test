14aeab0729dc7dc5320dcc904f00a2a9949bd42d
fqc <337940626@qq.com>
2015-12-04 17:13:15 +0800

    定时任务 django_crontab

---
+++ website/calc/cron.py
@@ -0,0 +1,9 @@
+def testschedule():
+    fsock = open("/Users/fqc/t.py", "a")
+    fsock.write("""
+    #Line 1 Just for test purpose
+    #Line 2 Just for test purpose
+    #Line 3 Just for test purpose
+    aaaa
+    """)
+    fsock.close()
